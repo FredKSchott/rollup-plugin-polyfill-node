6c42b4bc62df2a9324817efb898235c9c079f278
sosukesuzuki <aosukeke@gmail.com>
2021-03-08 23:57:35 +0900

    Format browser-test

--- browser-test/index.js
+++ browser-test/index.js
@@ -1,20 +1,22 @@
-var rollup = require( 'rollup' );
+var rollup = require('rollup');
 var nodePolyfills = require('..');
-rollup.rollup({
-  input: 'browser-test/main.js',
-  plugins: [
-    nodePolyfills(),
-  ]
-}).then( function ( bundle ) {
-  return bundle.write({
-    format: 'iife',
-    file: 'browser-test/dist/bundle.js'
+rollup
+  .rollup({
+    input: 'browser-test/main.js',
+    plugins: [nodePolyfills()],
+  })
+  .then(function (bundle) {
+    return bundle.write({
+      format: 'iife',
+      file: 'browser-test/dist/bundle.js',
+    });
+  })
+  .then(function () {
+    console.log('done');
+    process.exit();
+  })
+  .catch(function (e) {
+    console.log('oh noes!');
+    console.log(e);
+    process.exit(1);
   });
-}).then(function () {
-  console.log('done');
-  process.exit();
-}).catch(function (e) {
-  console.log('oh noes!');
-  console.log(e);
-  process.exit(1);
-});
--- browser-test/main.js
+++ browser-test/main.js
@@ -5,24 +5,27 @@ import {equal, deepEqual} from 'assert';
 get('foo.json', function (res) {
   console.log('status', res.statusCode);
   var data = '';
-  res.on('data', function (d) {
-    data += d.toString();
-  }).on('error', function (e) {
-    console.log('error', e);
-  }).on('end', function () {
-    console.log(data);
-    if (global.document) {
-      afterMain();
-    } else {
-      afterWorker();
-    }
-  });
-})
+  res
+    .on('data', function (d) {
+      data += d.toString();
+    })
+    .on('error', function (e) {
+      console.log('error', e);
+    })
+    .on('end', function () {
+      console.log(data);
+      if (global.document) {
+        afterMain();
+      } else {
+        afterWorker();
+      }
+    });
+});
 function afterMain() {
   var context = createContext();
 
   runInContext('var x = 1', context);
-  deepEqual(context, { x: 1 });
+  deepEqual(context, {x: 1});
 
   runInContext('var y = 2;', context);
   var x = runInContext('++x', context);
@@ -35,7 +38,7 @@ function afterWorker() {
   var context = createContext({x: 0});
 
   runInContext('x++', context);
-  deepEqual(context, { x: 1 });
+  deepEqual(context, {x: 1});
 
   var x = runInContext('++x', context);
   equal(x, 2);
